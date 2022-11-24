b05a424ee182f56eac7f5c965ab08440e2fa68b1
Maxim <maksim.grincevich@gmail.com>
2022-11-25 00:06:45 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,32 +3,28 @@ import {
   Nav, Navbar, Container,
 } from 'react-bootstrap';
 import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-// eslint-disable-next-line react/function-component-definition
 const App: React.FC = () => (
-  <div className="App">
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <img width={40} height={35} src="src/assets/logo.svg" />
-          INFOHAVE
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="navbar navbar-right">
-            <Nav.Link href="#home">Главная</Nav.Link>
-            <Nav.Link href="#favourite">
-              <img width={20} height={20} src="src/assets/heart.svg" />
-            </Nav.Link>
-            <Nav.Link href="#calendar">
-              <img width={20} height={20} src="src/assets/calendar.svg" />
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  </div>
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img width={40} height={35} src="src/assets/logo.svg" />
+        INFOHAVE
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="navbar navbar-right">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#favourite">
+            <img width={20} height={20} src="src/assets/heart.svg" />
+          </Nav.Link>
+          <Nav.Link href="#calendar">
+            <img width={20} height={20} src="src/assets/calendar.svg" />
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
 );
 
 export default App;
